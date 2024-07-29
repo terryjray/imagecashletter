71c619d05594e59f2f3b4ce559291a63f2267fa6
Terry Ray <tray@nxtsoft.com>
2024-07-29 14:38:52 -0500

    allow 0 in ArchiveTypeIndicator for FNBB

--- validators.go
+++ validators.go
@@ -382,7 +382,9 @@ func (v *validator) isArchiveTypeIndicator(code string) error {
 		// Electronic Check Instrument
 		"H",
 		// None
-		"I":
+		"I",
+		// FNBB hack
+		"0":
 		return nil
 	}
 	return errors.New(msgInvalid)
