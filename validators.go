195d1de5b80239d62f9b3498573953c3ad331aa7
Terry Ray <tray@finovifi.com>
2025-09-29 11:25:15 -0500

    remove condition patched upstream

--- validators.go
+++ validators.go
@@ -256,8 +256,6 @@ func (v *validator) isReturnsIndicator(code string) error {
 		"E",
 		// Customer–items being returned that directly affect a customer’s account.
 		"R",
-		// Jack Henry can't fix their own code
-		"N",
 		// Reject Return
 		"J",
 		// Altered/Fictitious Item/Suspected Counterfeit/Counterfeit
